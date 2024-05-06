9334de2227808aac95437d0aeab8818659473cce
Matthew <mrw2003x@gmail.com>
2024-05-06 16:58:16 -0700

    Added tab features to home page with js script and css file

---
+++ Portfolio_Project/HomePage/TabScript.js
@@ -0,0 +1,16 @@
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContent = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const tabType = document.querySelector(tab.dataset.tabTarget)
+        tabContent.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        })
+        tab.classList.add('active')
+        tabType.classlist.add('active')
+    })
+})
